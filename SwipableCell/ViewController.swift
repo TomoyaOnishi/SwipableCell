8eaab93b73c4c93015dea3b572640aae5219d222
TomoyaOnishi <tomoyaonishi@icloud.com>
2014-10-12 19:55:33 +0900

    Initial Commit

---
+++ SwipableCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipableCell
+//
+//  Created by TomoyaOnishi on 2014/10/12.
+//  Copyright (c) 2014年 TomoyaOnishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
